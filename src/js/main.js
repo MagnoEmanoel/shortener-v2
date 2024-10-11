59294122f1d7322eefaba9f6c5f0a56ca4ad31fb
Magno Emanoel <magno_emanoel@outlook.com>
2024-10-11 17:50:55 -0300

    Atualização qr-html e main.js

--- src/js/main.js
+++ src/js/main.js
@@ -61,11 +61,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function isValidUrl(url) {
-    const pattern = new RegExp('^(https?:\/\/)?' + // protocolo
-        '((([a-z\d]([a-z\d-]*[a-z\d])*)\.?)+[a-z]{2,}|' + // domínio
-        '((\d{1,3}\.){3}\d{1,3}))' + // IP (v4)
-        '(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // porta e caminho
-        '(\?[;&a-z\d%_.~+=-]*)?' + // query string
-        '(\#[-a-z\d_]*)?$', 'i'); // fragmento
+    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocolo
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domínio
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // IP (v4)
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // porta e caminho
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragmento
     return !!pattern.test(url);
 }
